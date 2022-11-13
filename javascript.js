12d5d0aeeec471654033ee004a3460dc670b41ec
NickHgstl <nickhoogst@gmail.com>
2022-11-14 00:26:16 +0100

    added rockPaperScissors function

--- javascript.js
+++ javascript.js
@@ -1,27 +1,60 @@
-
-
 function getRandomInt() {
     return Math.floor(Math.random() * 3)
     
 }
 
-
 let computerChoice = getRandomInt();
 
 function getComputerChoice(){
      if (computerChoice == 2)
-     console.log("rock")
+     return "rock"
         
 
      if (computerChoice == 1)
-     console.log("paper")
+     return "paper"
 
      if (computerChoice == 0)
-     console.log("scissors")
+     return "scissors"
 
      
         
     
 } 
 
-console.log(getComputerChoice());
+
+function rockPaperScissors(playerSelection, computerSelection) {
+   
+    playerSelection = prompt("Rock, paper or scissors?")
+    computerSelection = getComputerChoice()
+
+    if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "rock")
+        return "tie";
+
+        else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "paper")
+        return "paper wins";
+
+        else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "scissors")
+        return "rock wins";
+        
+    
+    if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "paper")
+        return "tie";
+
+        else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "rock")
+        return "paper wins";
+
+        else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "scissors")
+        return "scissors wins";
+
+    
+    if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "scissors")
+        return "tie";
+
+        else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "rock")
+        return "rock wins";
+
+        else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "paper")
+        return "scissors wins";
+}
+
+console.log(rockPaperScissors());
