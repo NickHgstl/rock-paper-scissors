99f282d1199a7979b35460231c8e37aa790f1049
NickHgstl <nickhoogst@gmail.com>
2022-11-13 23:28:49 +0100

    added function getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,4 +1,27 @@
-function getRandomInt(3) {
+
+
+function getRandomInt() {
     return Math.floor(Math.random() * 3)
-        if
+    
 }
+
+
+let computerChoice = getRandomInt();
+
+function getComputerChoice(){
+     if (computerChoice == 2)
+     console.log("rock")
+        
+
+     if (computerChoice == 1)
+     console.log("paper")
+
+     if (computerChoice == 0)
+     console.log("scissors")
+
+     
+        
+    
+} 
+
+console.log(getComputerChoice());
