551ddc44e7e927cb1696bdac40fcfdd0889543dc
NickHgstl <nickhoogst@gmail.com>
2022-11-27 17:46:01 +0100

    finished score and completed rock paper scissores

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let computerChoiche = 3;
-let computerWeapon = "paper";
+let computerWeapon = "";
 let computerWin = 0;
 let playerWin = 0;
 
@@ -10,7 +10,10 @@ function getRandomInt() {
 
 function getComputerChoiche(){
     if (getRandomInt(computerChoiche) == 0)
-        computerWeapon = "rock"
+        computerWeapon = "rock" 
+
+    if (getRandomInt(computerChoiche) == 1)
+        computerWeapon = "paper" 
 
     else if (getRandomInt(computerChoiche) == 2)
         computerWeapon = "scissors"
@@ -21,42 +24,54 @@ function playRound(){
     getComputerChoiche()
     let playerWeapon = prompt("rock paper or scissors?")
     
-    if (computerWeapon == "rock" && playerWeapon == "rock")
-    console.log("rock vs rock: draw")
+    if (computerWeapon == "rock" && playerWeapon == "rock"){
+        console.log("rock vs rock: draw")
+    }
 
-    if (computerWeapon == "rock" && playerWeapon == "paper")
-    console.log("paper vs rock: paper wins")
-    playerWin++
+    if (computerWeapon == "rock" && playerWeapon == "paper"){
+        console.log("paper vs rock: paper wins")
+        playerWin++
+    }
     
-    if (computerWeapon == "rock" && playerWeapon == "scissors")
-    console.log("scissors vs rock: scissors wins")
-    playerWin++
+    if (computerWeapon == "rock" && playerWeapon == "scissors"){
+        console.log("scissors vs rock: rock wins")
+        computerWin++
+    }
 
-    if (computerWeapon == "paper" && playerWeapon == "rock")
-    console.log("rock vs paper: paper wins")
+    if (computerWeapon == "paper" && playerWeapon == "rock"){
+        console.log("rock vs paper: paper wins")
+        computerWin++
+    }
 
-    if (computerWeapon == "paper" && playerWeapon == "paper")
-    console.log("paper vs paper: draw")
+    if (computerWeapon == "paper" && playerWeapon == "paper"){
+        console.log("paper vs paper: draw")
+    }
 
-    if (computerWeapon == "paper" && playerWeapon == "scissors")
-    console.log("scissors vs paper: scissors wins")
-    playerWin++
+    if (computerWeapon == "paper" && playerWeapon == "scissors"){
+        console.log("scissors vs paper: scissors wins")
+        playerWin++
+    }
 
-    if (computerWeapon == "scissors" && playerWeapon == "rock")
-    console.log("rock vs scissors: rock wins")
-    playerWin++
+    if (computerWeapon == "scissors" && playerWeapon == "rock"){
+        console.log("rock vs scissors: rock wins")
+        playerWin++
+    }
 
-    if (computerWeapon == "scissors" && playerWeapon == "paper")
-    console.log("paper vs scissors: scissors wins")
-    computerWin++
+    if (computerWeapon == "scissors" && playerWeapon == "paper"){
+        console.log("paper vs scissors: scissors wins")
+        computerWin++
+    }
 
-    if (computerWeapon == "scissors" && playerWeapon == "scissors")
-    console.log("scissors vs scissors: draw")
+    if (computerWeapon == "scissors" && playerWeapon == "scissors"){
+        console.log("scissors vs scissors: draw")
+    }
 }
 
 function score(){
-    playRound()
-    console.log(playerWin)
-    console.log(computerWin)
+    for (let i = 0; i < 5; i++){
+        playRound()
+        console.log(playerWin)
+        console.log(computerWin)
+    }
 }
 score()
